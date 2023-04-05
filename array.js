81d003ecb98d8e9d4200b796fb00af247d1c5bf8
Ashraful610 <ashalam610@gmail.com>
2023-04-05 20:33:33 +0600

    & and || condition

--- array.js
+++ array.js
@@ -5,7 +5,7 @@ var newArray = ['sadd','sabrin','foyad','rahim'];
 newArray[1] = 'korim'
 // console.log(newArray);
 var salary = 1000;
-var bouns = 500;
+var bouns = 50000;
 var cost = 11000;
 
 // if(salary < bouns ){
@@ -14,10 +14,16 @@ var cost = 11000;
 // else if( salary > bouns ){
 //     console.log('i got a bouns')
 // }
- if( cost > salary+bouns ){
-    console.log('Oh no , i cost more than my salary')
+//  if( salary > 500 && (cost > salary + bouns)){
+//     console.log('Oh no , i cost more than my salary')
+// }
+// else{
+//     console.log('finaly i have enough money')
+// }
+if (salary > cost || cost < (salary + bouns)){
+    console.log('ok , i can efort my family')
 }
 else{
-    console.log('finaly i have enough money')
+    console.log("i can't efort my family cost")
 }
 
