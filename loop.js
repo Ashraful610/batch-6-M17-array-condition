095d9763bcffd1d3564cc20a66cb59c40c766f71
Ashraful610 <ashalam610@gmail.com>
2023-04-05 22:53:21 +0600

    even and odd number loop

--- loop.js
+++ loop.js
@@ -1,5 +1,10 @@
-var number = 0;
-while(number < 25){
-    console.log(number , 'hello bangladesh')
-    number++
+// var number = 0;
+// while(number < 25){
+//     console.log(number , 'hello bangladesh')
+//     number++
+// }
+var num1 = 0;
+while(num1 <= 50){
+    console.log(num1);
+    num1 +=2 ;
 }
