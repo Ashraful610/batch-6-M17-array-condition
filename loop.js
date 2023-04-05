992f9b115b437bd150f4f144969ef19cb4d2b6c1
Ashraful610 <ashalam610@gmail.com>
2023-04-05 23:07:36 +0600

    for loop

--- loop.js
+++ loop.js
@@ -3,8 +3,14 @@
 //     console.log(number , 'hello bangladesh')
 //     number++
 // }
-var num1 = 0;
-while(num1 <= 50){
-    console.log(num1);
-    num1 +=2 ;
+// while loop
+// var num1 = 0;
+// while(num1 <= 50){
+//     console.log(num1);
+//     num1 +=2 ;
+// }
+
+// for loop
+for(var i = 1; i <= 10; i+=2) {
+    console.log(i)
 }
