33079d0275f9686cec52d36d2c57677713943e00
Ashraful610 <ashalam610@gmail.com>
2023-04-05 20:19:50 +0600

    if else condition

--- array.js
+++ array.js
@@ -3,4 +3,21 @@ var newArray = ['sadd','sabrin','foyad','rahim'];
 // newArray.push('hakim')
 // newArray.pop()
 newArray[1] = 'korim'
-console.log(newArray);
+// console.log(newArray);
+var salary = 1000;
+var bouns = 500;
+var cost = 11000;
+
+// if(salary < bouns ){
+//     console.log('yea i got a bouns more than salary ')
+// }
+// else if( salary > bouns ){
+//     console.log('i got a bouns')
+// }
+ if( cost > salary+bouns ){
+    console.log('Oh no , i cost more than my salary')
+}
+else{
+    console.log('finaly i have enough money')
+}
+
