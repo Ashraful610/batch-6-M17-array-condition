005d52a247ad7aea49cf73747928e7b129d81e5d
Ashraful610 <ashalam610@gmail.com>
2023-04-07 21:27:29 +0600

    total odd number in array and total even number in array  sum

--- oddEven.js
+++ oddEven.js
@@ -1,13 +1,13 @@
 // odd and even number
 
-const num1 = 23;
-const num2 = 44;
-if(num2 % 2 == 0){
-    console.log('this is even number', num2)
-}
-else{
-    console.log('this is odd number', num1)
-}
+// const num1 = 23;
+// const num2 = 44;
+// if(num2 % 2 == 0){
+//     console.log('this is even number', num2)
+// }
+// else{
+//     console.log('this is odd number', num1)
+// }
 
 // leapyear 
 function leapyear(year){
@@ -19,5 +19,16 @@ function leapyear(year){
         console.log('this is not leap year' , year)
     }
 }
-const happyFebruary = leapyear(2024)
-console.log(happyFebruary)
+// const happyFebruary = leapyear(2024)
+
+// return odd numbers in array =
+const numbersArray = [3, 5  , 7 , 9 , 13 , 17 , 20 , 12 , 25 , 24 , 44 , 82 , 6]
+let totalNumbers = 0;
+for(var i = 0 ; i < numbersArray.length ; i++){
+    const num1 = numbersArray[i]
+    if(num1 % 2 == 0){
+        totalNumbers += num1;
+        console.log(num1)
+    }
+}
+console.log(totalNumbers)
