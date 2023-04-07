c54ff625ec1fe6dddbd2be9424a6f90fd74e5e51
Ashraful610 <ashalam610@gmail.com>
2023-04-07 21:12:57 +0600

    leap year function

--- oddEven.js
+++ oddEven.js
@@ -1,3 +1,5 @@
+// odd and even number
+
 const num1 = 23;
 const num2 = 44;
 if(num2 % 2 == 0){
@@ -6,3 +8,16 @@ if(num2 % 2 == 0){
 else{
     console.log('this is odd number', num1)
 }
+
+// leapyear 
+function leapyear(year){
+    const year2 = year % 4;
+    if(year2 == 0){
+    console.log("this is leap year" , year2)
+    }
+    else{
+        console.log('this is not leap year' , year)
+    }
+}
+const happyFebruary = leapyear(2024)
+console.log(happyFebruary)
