1e295ebc66847d0e83f7a883c91339e305841fcd
Ashraful610 <ashalam610@gmail.com>
2023-04-07 20:36:56 +0600

    inches to feet and miles to killometers

---
+++ inchesToFeet.js
@@ -0,0 +1,20 @@
+// const inches = 84;
+// const feet = inches/ 12
+
+// inches to feet
+
+function inchesFeet(inches){
+    const feet = inches / 12
+    return feet;
+}
+const feet = inchesFeet(180)
+console.log( feet)
+
+
+// miles to kilometers
+function milesToKm(miles){
+    const killometers = miles * 1.609
+    return killometers
+}
+const killometers = milesToKm(3)
+console.log(killometers)
