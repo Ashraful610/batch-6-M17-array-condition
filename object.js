af847de57b52b470dba000c793ab1ed665924419
Ashraful610 <ashalam610@gmail.com>
2023-04-07 15:41:04 +0600

    object keys and values

---
+++ object.js
@@ -0,0 +1,21 @@
+var myComputer = {
+    name:'HP',
+    corei:'i5',
+    ram:'8gb',
+    rom:'256gb'
+}
+// console.log(myComputer)
+// myComputer.corei = 'i7'
+// console.log(myComputer);
+
+// var properties = Object.keys(myComputer)
+// var values = Object.values(myComputer);
+// var keyValuesPairs = Object.entries(myComputer)
+
+// console.log(properties)
+// console.log(values)
+// console.log(keyValuesPairs)
+
+// var name = myComputer.name;
+var ram = myComputer['ram']
+console.log(ram)
